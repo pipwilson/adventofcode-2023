5ec73eba07d48f2ac705924b85cfd7ee0776bb8f
Phil Wilson <github@philwilson.org>
2023-12-03 15:12:32 +0000

    Not passing part 2 of day 1 still :(

--- 01/DayOne.php
+++ 01/DayOne.php
@@ -1,13 +1,16 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 final class DayOne
 {
-    
+
     public function getFirstNumberFromLine($line): int
     {
-        $firstNumberPattern = "(\d)";
+        $firstNumberPattern = "(one|two|three|four|five|six|seven|eight|nine|\d)";
         if (preg_match($firstNumberPattern, $line, $matches) == 1) {
-            return (int)$matches[0];
+            $match = $matches[0];
+            return $this->convertMatch($match);
         } else {
             return -1;
         }
@@ -15,9 +18,41 @@ final class DayOne
 
     public function getLastNumberFromLine($line): int
     {
-        $lastNumberPattern = "/(\d)/";
+        $lastNumberPattern = "/(one|two|three|four|five|six|seven|eight|nine|\d)/";
         if (preg_match_all($lastNumberPattern, $line, $matches) > 0) {
-            return (int)$matches[0][count($matches[0])-1];
+            $match = $matches[0][count($matches[0]) - 1];
+            return $this->convertMatch($match);
+        } else {
+            return -1;
+        }
+    }
+
+    public function convertMatch($match): int
+    {
+        // regex matches are always strings, so we have to use is_numeric and cast, rather than is_int
+        if (is_numeric($match)) {
+            return (int)$match;
+        } else if (is_string($match)) {
+            return $this->convertWordToNumber($match);
+        }
+    }
+
+    public function convertWordToNumber($word): int
+    {
+        $mappingArray = [
+            'one' => 1,
+            'two' => 2,
+            'three' => 3,
+            'four' => 4,
+            'five' => 5,
+            'six' => 6,
+            'seven' => 7,
+            'eight' => 8,
+            'nine' => 9,
+        ];
+
+        if (array_key_exists($word, $mappingArray)) {
+            return $mappingArray[$word];
         } else {
             return -1;
         }
@@ -25,24 +60,22 @@ final class DayOne
 
     public function readCalibrationFile(): void
     {
-        $lines = file('calibrationValues.txt', FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
+        $lines = file('calibrationValues.txt', FILE_SKIP_EMPTY_LINES);
 
         $total = 0;
-        
-        foreach($lines as $line) {
-              
+
+        foreach ($lines as $line) {
+
             $firstValue = $this->getFirstNumberFromLine($line);
             $lastValue = $this->getLastNumberFromLine($line);
-            if($firstValue < 0 || $lastValue < 0) {
-                exit('Did not find the values in line: '.$line);
+            if ($firstValue < 0 || $lastValue < 0) {
+                exit('Did not find the values in line: ' . $line);
             } else {
-                $calibrationValue = (int)($firstValue.$lastValue);
+                $calibrationValue = (int)($firstValue . $lastValue);
                 $total = $total + $calibrationValue;
             }
         }
 
         print $total;
-        
     }
-
 }
--- 01/DayOneTest.php
+++ 01/DayOneTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 final class DayOneTest extends TestCase
 {
+ 
     public function testGetFirstNumberFromLine(): void
     {
         $line = "XX322";
@@ -44,6 +45,123 @@ final class DayOneTest extends TestCase
         $lastNumber = $dayOne->getLastNumberFromLine($line);
 
         $this->assertEquals(7, $lastNumber);
+    }
+
+    public function testGetFirstNumberFromMixedString(): void
+    {
+        $line = "4nineeightseven2";
+
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+
+        $this->assertEquals(4, $firstNumber);
+    }
+
+    public function testGetLastNumberFromMixedString(): void
+    {
+        $line = "4nineeightseven2";
+
+        $dayOne = new DayOne;
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $this->assertEquals(2, $lastNumber);
+    }
+
+    public function testGetFirstNumberFromLongMixedString(): void
+    {
+        $line = "oneeighteightseven2";
+
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+
+        $this->assertEquals(1, $firstNumber);
+    }
+
+    public function testGetLastNumberFromLongMixedString(): void
+    {
+        $line = "oneeighteightseven";
+
+        $dayOne = new DayOne;
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $this->assertEquals(7, $lastNumber);
+    }
+
+    public function testBuildFullNumber1(): void
+    {
+        $line = "two1nine";
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $fullNumber = (int)($firstNumber . $lastNumber);
+        $this->assertEquals(29, $fullNumber);
+    }
+
+    public function testBuildFullNumber2(): void
+    {
+        $line = "eightwothree";
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $fullNumber = (int)($firstNumber . $lastNumber);
+        $this->assertEquals(83, $fullNumber);
+    }
+
+    public function testBuildFullNumber3(): void
+    {
+        $line = "abcone2threexyz";
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $fullNumber = (int)($firstNumber . $lastNumber);
+        $this->assertEquals(13, $fullNumber);
+    }
+
+    public function testBuildFullNumber4(): void
+    {
+        $line = "xtwone3four";
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $fullNumber = (int)($firstNumber . $lastNumber);
+        $this->assertEquals(24, $fullNumber);
+    }
+    
+    public function testBuildFullNumber5(): void
+    {
+        $line = "4nineeightseven2";
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $fullNumber = (int)($firstNumber . $lastNumber);
+        $this->assertEquals(42, $fullNumber);
+    }
+
+    public function testBuildFullNumber6(): void
+    {
+        $line = "zoneight234";
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $fullNumber = (int)($firstNumber . $lastNumber);
+        $this->assertEquals(14, $fullNumber);
+    }
+
+    public function testBuildFullNumber7(): void
+    {
+        $line = "7pqrstsixteen";
+        $dayOne = new DayOne;
+        $firstNumber = $dayOne->getFirstNumberFromLine($line);
+        $lastNumber = $dayOne->getLastNumberFromLine($line);
+
+        $fullNumber = (int)($firstNumber . $lastNumber);
+        $this->assertEquals(76, $fullNumber);
     }
 
 }
