dbdd614d286c7fbb6490b7cc5a4d3ab7e1baa2f6
Phil Wilson <github@philwilson.org>
2023-12-13 17:46:07 +0000

    Day 4 Part 1 working

--- 04/DayFour.php
+++ 04/DayFour.php
@@ -7,4 +7,33 @@ final class DayFour
     {
         return -1;
     }
+
+    public function getScratchCards($filename): int
+    {
+        $lines = file($filename, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+        $cardScore = 0;
+
+        foreach ($lines as $scratchcard) {
+            $lineScore = 0;
+            $numberOfMatches = 0;
+            $scratchcard = explode(": ", $scratchcard)[1];
+            $numbers = explode(' | ', $scratchcard);
+            $winningNumbers = preg_split("/\s+/", $numbers[0], -1, PREG_SPLIT_NO_EMPTY);
+            $myNumbers = preg_split("/\s+/", $numbers[1], -1, PREG_SPLIT_NO_EMPTY);
+
+            foreach ($myNumbers as $myNumber) {
+                if(in_array($myNumber, $winningNumbers)) {
+                    $numberOfMatches++;
+                }
+            }
+            if($numberOfMatches > 0) {
+                $lineScore = pow(2, $numberOfMatches-1);
+            }
+
+            $cardScore += $lineScore;
+        }
+
+        return $cardScore;
+
+    }
 }
