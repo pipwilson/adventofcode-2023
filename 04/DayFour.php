5d0502c864747069d632ae767c0a0357a4511cef
Phil Wilson <github@philwilson.org>
2023-12-17 00:47:45 +0000

    Day 4 Part 2 working

--- 04/DayFour.php
+++ 04/DayFour.php
@@ -3,29 +3,50 @@
 final class DayFour
 {
 
-    public function getScratchcardPoints($filename): int
+    public function partTwo($filename): void
     {
-        return -1;
+        $lines = file($filename, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+        $cards = array();
+        foreach ($lines as $line) {
+            $card["line"] = $line;
+            $card["count"] = 1;
+            $cards[] = $card;
+        }
+        unset($card);
+        unset($line);
+
+        foreach ($cards as $index=>&$card) {
+            for ($i = 0; $i < $card['count']; $i++) {
+                $this->processScratchcard($cards, $index,$card);
+            }
+        }
+        unset($card);
+
+        $sum=0;
+
+        foreach($cards as $card) {
+            $sum+=$card['count'];
+        }
+
+        echo $sum;
     }
 
-    public function getScratchCards($filename): int
+    function processScratchcard(&$cards, $index, $card) : void
+    {
+        $numberOfMatches = $this->processScratchcardLine($card['line']);
+        for ($i = 1; $i <= $numberOfMatches; $i++) {
+            $cards[$index+$i]['count']++;
+        }
+    }
+
+    public function getScratchcards($filename): int
     {
         $lines = file($filename, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
         $cardScore = 0;
 
         foreach ($lines as $scratchcard) {
             $lineScore = 0;
-            $numberOfMatches = 0;
-            $scratchcard = explode(": ", $scratchcard)[1];
-            $numbers = explode(' | ', $scratchcard);
-            $winningNumbers = preg_split("/\s+/", $numbers[0], -1, PREG_SPLIT_NO_EMPTY);
-            $myNumbers = preg_split("/\s+/", $numbers[1], -1, PREG_SPLIT_NO_EMPTY);
-
-            foreach ($myNumbers as $myNumber) {
-                if(in_array($myNumber, $winningNumbers)) {
-                    $numberOfMatches++;
-                }
-            }
+            $numberOfMatches = $this->processScratchcardLine($scratchcard);
             if($numberOfMatches > 0) {
                 $lineScore = pow(2, $numberOfMatches-1);
             }
@@ -35,5 +56,22 @@ final class DayFour
 
         return $cardScore;
 
+    }
+
+    public function processScratchcardLine($scratchcard): int
+    {
+
+        $numberOfMatches = 0;
+        $scratchcard = explode(": ", $scratchcard)[1];
+        $numbers = explode(' | ', $scratchcard);
+        $winningNumbers = preg_split("/\s+/", $numbers[0], -1, PREG_SPLIT_NO_EMPTY);
+        $myNumbers = preg_split("/\s+/", $numbers[1], -1, PREG_SPLIT_NO_EMPTY);
+
+        foreach ($myNumbers as $myNumber) {
+            if (in_array($myNumber, $winningNumbers)) {
+                $numberOfMatches++;
+            }
+        }
+        return $numberOfMatches;
     }
 }
--- 04/DayFourRunner.php
+++ 04/DayFourRunner.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
     require_once 'DayFour.php';
     $dayFour = new DayFour;
-    print($dayFour->getScratchcards('scratchcard.txt')."\n");
+    print($dayFour->getScratchcards('example-scratchcard.txt')."\n");
+    print($dayFour->partTwo('scratchcard.txt')."\n");
+
 ?>
